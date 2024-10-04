c6062cd7536ede5ac8d8aa808bf7817600ccc8b9
Gonza-L <leyesgonzalomatias@gmail.com>
2024-10-03 22:21:14 -0300

    meme added

---
+++ components/meme/meme.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def meme_display():
+    # Set the expander
+    with st.expander("Meme"):
+        # Set the subheader
+        st.subheader("Siguiente")
+
+        # Display the image
+        st.image("components/meme/imgs/meme.png", caption="Meme Image", use_column_width=True)
